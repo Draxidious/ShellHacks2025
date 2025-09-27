1d88b1aba7154714da4e38e50143f8ba434f82a0
Guufla <liamabramov@gmail.com>
2025-09-27 12:34:49 -0400

    Board mechanics

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,13 +1,20 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.Services.Lobbies.Models;
 
 public class BoardManager : MonoBehaviour
 {
+    // Singleton instance for easy global access
+    public static BoardManager Instance { get; private set; }
+
     #region Board variables
+    public List<GameObject> boardSpaceObjects;
+
     public List<Transform> boardSpacePositions;
 
     #endregion
-    
+
     #region Player variables
     public List<GameObject> playerPieces;
 
@@ -17,12 +24,68 @@ public class BoardManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GetBoardSpacePositions();
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        GameManager.OnPlayerMoveRequested += HandlePlayerMoveRequested;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnPlayerMoveRequested -= HandlePlayerMoveRequested;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void MovePlayerPiece(int playerIndex, int spaceMovement)
+    {
+        if (playerIndex < playerPieces.Count &&  playerIndex + spaceMovement < boardSpacePositions.Count)
+        {
+            playerPieces[playerIndex].transform.position = boardSpacePositions[playerIndex + spaceMovement].position;
+        }
+        else
+        {
+            playerPieces[playerIndex].transform.position = boardSpacePositions[playerIndex + spaceMovement - boardSpacePositions.Count].position;
+        }
+    }
+
+    private void HandlePlayerMoveRequested(int playerIndex, int boardSpaceIndex)
+    {
+        MovePlayerPiece(playerIndex, boardSpaceIndex);
+    }
+    public static Action<int> OnRequestTileTransform;
+    public Transform RequestTileTransform(int tileIndex)
+    {
+        OnRequestTileTransform?.Invoke(tileIndex);
+        //boardSpaceObjects[tileIndex].GetComponent<TileInformation>().GetPiecePosition();
+    }
+
+    
+    void GetBoardSpacePositions()
+    {
+        boardSpacePositions = new List<Transform>();
+
+        foreach (GameObject space in boardSpaceObjects)
+        {
+            boardSpacePositions.Add(space.transform);
+        }
     }
 }
---
+++ Assets/Scripts/TileInformation.cs
@@ -0,0 +1,59 @@
+using TMPro;
+using UnityEngine;
+
+public class TileInformation : MonoBehaviour
+{
+    #region Tile variables
+
+    [SerializeField] Transform PiecePosition;
+
+    [SerializeField] int CurrentSpaceIndex;
+    // public Material sectionColor;
+
+    // public string locationName;
+
+    // public string currentOwner;
+
+    // public float purchasePrice;
+
+    [SerializeField] TextMeshPro locationNameText;
+
+    [SerializeField] TextMeshPro currentOwnerText;
+
+    [SerializeField] TextMeshPro purchasePriceText;
+
+    #endregion
+
+    private void OnEnable()
+    {
+        BoardManager.RequestTileTransform += GetPiecePosition();
+    }
+
+    private void OnDisable()
+    {
+        BoardManager.RequestTileTransform -= GetPiecePosition();
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public Transform GetPiecePosition()
+    {
+        return PiecePosition;
+    }
+    public int GetSpaceIndex()
+    {
+        return CurrentSpaceIndex;
+    }
+}
