bd30f1168a8303c76a782e10cf371b20d15ecca0
Guufla <liamabramov@gmail.com>
2025-09-27 10:40:27 -0400

    Initial setup

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    #region Board variables
+    Public List<Transform> boardSpacePositions;
+    
+    #region Player variables
+    Public List<GameObject> playerPieces;
+    
+    #endregion
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
